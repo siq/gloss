91856b505b04245517f21cd20e0d0413160d6c8e
Ralph Smith <ralphsmith80@gmail.com>
2013-02-15 13:18:30 -0600

    add focus function so grid focuses on selected model

--- src/widgets/powergrid/askeyboardnavigatable.js
+++ src/widgets/powergrid/askeyboardnavigatable.js
@@ -3,34 +3,6 @@ define([
 ], function(_) {
     return function() {
 
-        // var self = this;
-        // _.wrap(this.init, function(init) {
-        //     if (!self.get('keyboardNavigation')) {
-        //         return;
-        //     }
-        //     self.$el.bind('keyup', 'tbody tr', function(evt) {
-        //         var selectModel, selectIndex,
-        //             selected = self.selected(),
-        //             models = self.get('models');
-
-        //         if (!selected || !self.$rowInnerWrapper.is(':visible')) {
-        //             return;
-        //         }
-        //         if (evt.which === 13) {      //  - enter key
-        //             self._trFromModel(selected).trigger('dblclick');
-        //             console.log('trigger');
-        //             return;
-        //         } else if (evt.which === 38) {             //  - up arrow
-        //             selectIndex = models.indexOf(selected) - 1;
-        //         } else if (evt.which === 40) {      //  - down arrow
-        //             selectIndex = models.indexOf(selected) + 1;
-        //         }
-        //         selectModel = models[selectIndex];
-        //         if (selectModel) {
-        //             self.select(selectModel);
-        //         }
-        //     });
-        // });
         var _bindKeyboardNavigation = function() {
             var self = this;
             if (!this.get('keyboardNavigation')) {
@@ -50,9 +22,7 @@ define([
             this.$el.bind('keyup', 'tbody tr', function(evt) {
                 var selectModel, selectIndex,
                     selected = self.selected(),
-                    models = self.get('models'),
-                    scrollTop = 0,
-                    trHeight = self.$rowInnerWrapper.find('tr').first().height();
+                    models = self.get('models');
 
                 if (!selected || !self.$rowInnerWrapper.is(':visible')) {
                     return;
@@ -62,26 +32,60 @@ define([
                     return;
                 } else if (evt.which === 38) {             //  - up arrow
                     selectIndex = models.indexOf(selected) - 1;
-                    scrollTop = self.$rowInnerWrapper.scrollTop() - trHeight;
                 } else if (evt.which === 40) {      //  - down arrow
                     selectIndex = models.indexOf(selected) + 1;
-                    scrollTop = self.$rowInnerWrapper.scrollTop() + trHeight;
                 }
-                selectModel = models[selectIndex];
-                if (selectModel) {
-                    self.select(selectModel);
-                    var top = self._trFromModel(selectModel).position().top,
-                        gridHeight = self.$rowInnerWrapper.height();
-                    if (top > gridHeight || top < trHeight) {
-                        self.$rowInnerWrapper.scrollTop(scrollTop);
-                    }
-                }
+                selectedModel = models[selectIndex] || selected;
+                self.select(selectedModel);
+                self._focusModel(selectedModel);
             });
+        };
+        var _rerenderExtend = function () {
+            var self = this, _rerender = this._rerender;
+            if (!this.get('keyboardNavigation')) {
+                return;
+            }
+            this._rerender = function() {
+                var selected = self.selected();
+                _rerender.call(self);
+                if (selected) {
+                    self._focusModel(selected);
+                }
+            };
+        };
+        this._focusModel = function(model) {
+            var models = this.get('models'),
+                headerHeight = this.$el.find('.header-wrapper').height(),
+                trHeight = this.$rowInnerWrapper.find('tr').first().height(),
+                scrollTop = this.$rowInnerWrapper.scrollTop(),
+                scrollTo;
+
+            model = model || _.first(models);
+
+            if (_.last(models) === model) {
+                // - this is the last row so just scroll to the bottom
+                scrollTo = this.$rowInnerWrapper.find('.rows').height();
+            } else if (_.first(models) === model) {
+                // - this is the first row so just scroll to the top
+                scrollTo = 0;
+            } else {
+                var top = this._trFromModel(model).position().top,
+                    gridHeight = this.$rowInnerWrapper.height();
+                if (top < trHeight) { // - row is above the grid view
+                    scrollTo = scrollTop - headerHeight + top;
+                } else if (top > gridHeight) { //  - row is below the grid view
+                    scrollTo = scrollTop - gridHeight + top;
+                }
+            }
+            if (typeof scrollTo === 'number') {
+                this.$rowInnerWrapper.scrollTop(scrollTo);
+            }
         };
 
         if(!this.afterInit) {
             this.afterInit = [];
         }
         this.afterInit.push(_bindKeyboardNavigation);
+        this.afterInit.push(_rerenderExtend);
     };
 });
