c9fe9888521782cb6ce7dcae0dfa9685991a25e3
Ralph Smith <ralphsmith80@gmail.com>
2013-02-15 14:20:10 -0600

    keybinding is no longer a mixin

--- src/widgets/powergrid.js
+++ src/widgets/powergrid.js
@@ -149,6 +149,45 @@ define([
                     scrollLoadDfd = collection.load().then(function(models) {});
                 }
             });
+            if (this.get('keyboardNavigation')) {
+                this._bindKeyboardNavigation();
+            }
+        },
+
+        _bindKeyboardNavigation: function() {
+            var self = this;
+
+            //  - We don't want the page to scroll when were trying to navigate
+            //  - with the keyboard so we're going to prevent that here.
+            var keys = [38,40];
+            this.$el.bind('keydown', 'tbody tr', function(evt) {
+                var key = evt.which;
+                if(_.contains(keys, key)) {
+                    evt.preventDefault();
+                    return false;
+                }
+                return true;
+            });
+            this.$el.bind('keyup', 'tbody tr', function(evt) {
+                var selectModel, selectIndex,
+                    selected = self.selected(),
+                    models = self.get('models');
+
+                if (!selected || !self.$rowInnerWrapper.is(':visible')) {
+                    return;
+                }
+                if (evt.which === 13) {      //  - enter key
+                    self._trFromModel(selected).trigger('dblclick');
+                    return;
+                } else if (evt.which === 38) {             //  - up arrow
+                    selectIndex = models.indexOf(selected) - 1;
+                } else if (evt.which === 40) {      //  - down arrow
+                    selectIndex = models.indexOf(selected) + 1;
+                }
+                selectedModel = models[selectIndex] || selected;
+                self.select(selectedModel);
+                self._focusModel(selectedModel);
+            });
         },
 
         _focusModel: function(model) {
@@ -564,7 +603,6 @@ define([
     });
 
     asCollectionViewable.call(PowerGrid.prototype);
-    asKeyboardNavigatable.call(PowerGrid.prototype);
 
     return PowerGrid;
 });
