d8273aee31e97195841b29644f38134dd4e9b438
Ralph Smith <ralphsmith80@gmail.com>
2013-03-04 10:29:39 -0600

    binding a synthetic submit when root node != form

--- src/widgets/powergrid/powergridsearch.js
+++ src/widgets/powergrid/powergridsearch.js
@@ -19,46 +19,30 @@ define([
             if (!this.getWidget('q').getValue()) {
                 this.getWidget('clear').disable();
             }
-            // this.on('submit', this.submit);
-            // - this is more complicated than it needs to be at this point
-            //  we need to be able to put the searcher in a form or a div
-            //  we may want to just say this need to be a div
-            // - IE doesn't like it to be just a div ... it won't submit with
-            // just the enter button.
-            //   One way to resolve this is to leave the onSumbit event handler inplace
-            // but since we have to catch the enter key for FF we might be able to do
-            // that in IE too.
-            // - Firefox has issues submitting on the right <input> when it's in a div
-            // for example in the third pane of the create-matter modal it will submit
-            // on the doclist input which will be the incorrect value for the filter.
-            //   To fix this we have to catch the enter key from the input and prevent
-            // the default submit and do what we want.
-
-            // - WORKS!!! this works in IE8, FF, Chrome
-            var self = this;
-            // if (this.$node.is('form')) {
-            //     this.on('submit', function(evt) {
-            //         evt.preventDefault();
-            //         console.log('submit');
-            //         self.submit(evt);
-            //     });
-            // }
-            // if (this.$node.is('div')) {
-                this.on('click', '[name=search]', function(evt) {
-                    evt.preventDefault();
-                    console.log('click');
-                    self.submit(evt);
-                });
-                this.on('keydown', '[name=q]', function(evt) {
-                    if (evt.which === 13) {
-                        evt.preventDefault();
-                        self.submit(evt);
-                    }
-                });
-            // }
+            this.on('submit', this.submit);
+            // we need to handle the submit event synthetically when the
+            // searchers root node is NOT a form.
+            // this is usually the case when we have to put the searcher in parent form
+            // since a form can not be in a form.
+            if (!this.$node.is('form')) {
+                this._bindSyntheticSubmit();
+            }
             this.on('keyup', '[name=q]', this._onKeyup);
             this.on('click', '[name=clear]', this._onClickClear);
             this.update();
+        },
+        _bindSyntheticSubmit: function() {
+            var self = this;
+            this.on('click', '[name=search]', function(evt) {
+                evt.preventDefault();
+                self.submit(evt);
+            });
+            this.on('keydown', '[name=q]', function(evt) {
+                if (evt.which === 13) {
+                    evt.preventDefault();
+                    self.submit(evt);
+                }
+            });
         },
         _getPreviousParams: function() {
             return $.extend(true, {}, this.options.collection.query.params);
