2dd016c1d6f1604defb1aa156e4a2a242e75672b
Aaron Stacy <aaron.r.stacy@gmail.com>
2013-03-20 17:58:15 -0500

    handle checkbox columns in  powergrid asformwidget
    this makes the `asFormWidget` mixins intelligently handle the case where
    the grid has a checkbox column. it also adds support for multi-select,
    either through checkbox columns or multiselection.

--- src/widgets/powergrid/asformwidget.js
+++ src/widgets/powergrid/asformwidget.js
@@ -16,22 +16,43 @@ define([
         });
 
         this._onChangeFormValue = function(evtName, xhr, model, changed) {
-            var prop = this._asFormWidgetSelectedProp();
-            if (changed[prop]) {
+            var selectedProp = this._asFormWidgetSelectedProp();
+            if (changed[selectedProp]) {
                 this.trigger('change');
             }
         };
 
+        this._asFormWidgetCheckBoxColumn = function() {
+            return _.find(this.get('columnModel').columns, function(c) {
+                return c instanceof CheckBoxColumn;
+            });
+        };
+
+        this._asFormWidgetIsMultiSelect = function() {
+            var cb = this._asFormWidgetCheckBoxColumn();
+            return cb?
+                cb.get('type') === 'checkbox' :
+                this.get('selectable') === 'multi';
+        };
+
         this._asFormWidgetSelectedProp = function() {
+            var cb;
             if (this._asFormWidgetSelectedPropName) {
                 return this._asFormWidgetSelectedPropName;
             }
-            var cb = _.find(this.get('columnModel').columns, function(c) {
-                return c instanceof CheckBoxColumn;
-            });
-            return cb?
+            return (cb = this._asFormWidgetCheckBoxColumn())?
                 this._asFormWidgetSelectedPropName = cb.get('prop') :
                 this._asFormWidgetSelectedPropName = this.get('selectedAttr');
+        };
+
+        this._asFormWidgetSelected = function() {
+            var selectedProp = this._asFormWidgetSelectedProp(),
+                cb = this._asFormWidgetCheckBoxColumn(),
+                multi = this._asFormWidgetIsMultiSelect(),
+                where = multi? 'where' : 'findWhere';
+            return cb?
+                _[where](this.get('models'), selectedProp, true) :
+                this.selected();
         };
 
         this.clearStatus = function(opts) {
@@ -43,8 +64,9 @@ define([
         };
 
         this.getValue = function() {
-            var m = this.selected();
-            return m && m.get(prop);
+            var multi = this._asFormWidgetIsMultiSelect(),
+                m = this._asFormWidgetSelected();
+            return multi? m && _.mpluck(m, prop) : m && m.get(prop);
         };
 
         this.setStatus = function(type, msg) {
@@ -59,27 +81,59 @@ define([
             return this;
         };
 
-        this.setValue = function(newValue) {
-            var attr = this.get('selectedAttr');
-            if ((_.isArray(newValue) && newValue.length === 0) ||
-                    newValue === void 0) {
-                this.unselect();
-            } else {
-                newValue = _.isArray(newValue)? newValue : [newValue];
-                this.select(_.find(this.get('models'), function(m) {
-                    return _.indexOf(newValue, m.get(prop)) >= 0;
+        this._setValue = function(values) {
+            var changes, cb = this._asFormWidgetCheckBoxColumn(),
+                selectedProp = this._asFormWidgetSelectedProp(),
+                models = _.filter(this.get('models'), function(m) {
+                    return _.indexOf(values, m.get(prop)) >= 0;
+                });
+            if (cb) {
+                changes = _.compact(_.map(this.get('models'), function(m) {
+                    if (!cb._isDisabled(m)) {
+                        if (values.indexOf(m.get(prop)) >= 0) {
+                            if (!m.get(selectedProp)) {
+                                return {model: m, value: true};
+                            }
+                        } else {
+                            if (m.get(selectedProp)) {
+                                return {model: m, value: false};
+                            }
+                        }
+                    }
                 }));
+                this._disableRerender = true;
+                _.each(changes, function(change, i) {
+                    change.model.set(selectedProp, change.value, {
+                        silent: i+1 < changes.length
+                    });
+                });
+                this._disableRerender = false;
+                if (changes.length > 0) {
+                    if (changes.length > 2) {
+                        this.rerender();
+                    } else {
+                        _.map(_.pluck(changes, 'model'), this.rerender);
+                    }
+                }
+            } else {
+                this.select(models);
             }
+        };
+
+        this.setValue = function(newValue) {
+            this._setValue(_.isArray(newValue)? newValue : [newValue]);
             return this;
         };
 
         this.update = _.wrap(this.update, function(update, changed) {
             if (changed.collection) {
                 if (this.previous('collection')) {
-                    this.previous('collection').off('change', this._onChangeFormValue);
+                    this.previous('collection')
+                        .off('change', this._onChangeFormValue);
                 }
                 if (this.get('collection')) {
-                    this.get('collection').on('change', this._onChangeFormValue);
+                    this.get('collection')
+                        .on('change', this._onChangeFormValue);
                 }
             }
             return update.apply(this, _.rest(arguments, 1));
