e091a387c9460e9e5f1feca88f350bde09319665
Ralph Smith <ralphsmith80@gmail.com>
2013-04-10 10:22:01 -0500

    add unit tests for xss

--- src/require/test/test_tmpl.js
+++ src/require/test/test_tmpl.js
@@ -1,8 +1,9 @@
 /*global test, asyncTest, ok, equal, deepEqual, start, module, strictEqual */
 define([
     'vendor/jquery',
-    'tmpl!./template.mtpl'
-], function($, tmpl) {
+    'tmpl!./template.mtpl',
+    'tmpl!./xss_tmpl.mtpl'
+], function($, tmpl, xss_tmpl) {
     test('template plugin compiled', function() {
         ok(tmpl);
         var data = {
@@ -13,6 +14,34 @@ define([
         equal($html.find('li').length, 3);
         equal($html.find('span:first').text(), data.foo.bar);
         ok(/uno.*dos.*tres/.test($html.find('ul').text()));
+    });
+
+    // define some globals to check for xss execution
+    window.xssCount = 0;
+    window.increment = function() {
+            xssCount++;
+        };
+    test('template escapes html to prevent xss', function() {
+        xssCount = 0;
+        var badGuy = "<script>increment()</script>",
+            data = {
+                scripty: badGuy
+            },
+            $html = $(xss_tmpl(data));
+        // append to DOM so script will execute
+        $html.appendTo('body');
+        equal(xssCount, 0, 'template is excaping html');
+    });
+    test('template escape can be overridden', function() {
+        xssCount = 0;
+        var goodGuy = "<script>increment()</script>",
+            data = {
+                noscripty: goodGuy
+            },
+            $html = $(xss_tmpl(data));
+        // append to DOM so script will execute
+        $html.appendTo('body');
+        equal(xssCount, 1, 'template is excaping html');
     });
     start();
 });
