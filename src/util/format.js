46f301977e7bebe614856bc0d9f4745db7e5e927
Ralph Smith <ralphsmith80@gmail.com>
2013-04-30 11:47:29 -0500

    add license reference

--- src/util/format.js
+++ src/util/format.js
@@ -58,6 +58,28 @@ define([
 
     // this was taken from http://phpjs.org/functions/number_format/
     // the related github https://github.com/kvz/phpjs/blob/master/functions/strings/number_format.js
+    /*
+        Copyright (c) 2013 Kevin van Zonneveld (http://kvz.io) 
+        and Contributors (http://phpjs.org/authors)
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of
+        this software and associated documentation files (the "Software"), to deal in
+        the Software without restriction, including without limitation the rights to
+        use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+        of the Software, and to permit persons to whom the Software is furnished to do
+        so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+    */
     var number = function(number, decimals, decimalPoint, thousandsSep) {
         // http://kevin.vanzonneveld.net
         // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
