6c0aa3d78f893e16ca69fdaea93aafeda3bc4f51
cdablain <cad.yggdrasil@gmail.com>
2013-05-28 08:18:33 -0500

    DAQ-581: refactored page to be more testable, added unit tests for fade queue, cleaned up fadequeue.js

--- src/widgets/page/test.js
+++ src/widgets/page/test.js
@@ -5,24 +5,30 @@ define([
     'tmpl!./noArgTest.mtpl',
     'tmpl!./argTest.mtpl'
 ], function ($, Page, NoArgTmpl, ArgTmpl) {
+    QUnit.config.autostart = false;
 
-    test('Page instantiation test', function () {
-        var page = Page();
+    module( "page", {
+        setup: function() {
 
+        }, teardown: function() {
+            $('div.more-document-hinting').remove();
+        }
+    });
+
+    asyncTest('Page instantiation test', function () {
+        var page = Page(undefined, {disableHintingEvents: true});
         ok(page);
-
         start();
     });
-    test('Page micro template with no args test', function () {
+    asyncTest('Page micro template with no args test', function () {
         var page = Page(undefined, {
-            template: NoArgTmpl(null)
+            template: NoArgTmpl(null),
+            disableHintingEvents: true
         });
-
         ok(page);
-
         start();
     });
-    test('Page micro template with args test', function () {
+    asyncTest('Page micro template with args test', function () {
         var page,
             args = [
                 {text: 'One'},
@@ -34,53 +40,118 @@ define([
             ];
 
         page = Page(undefined, {
-            template: ArgTmpl(args)
+            template: ArgTmpl(args),
+            disableHintingEvents: true
         });
-
         ok(page);
-
         start();
     });
-    test('Page loaded event test', function() {
-        var page = Page();
+    asyncTest('Page loaded event test', function() {
+        var page = Page(undefined, {disableHintingEvents: true}); 
 
         ok(page);
         page.on('loaded', function() {
             ok("complete", document.readyState);
         });
+        start();
     });
-    test('Page load deferred test', function() {
-        var page = Page();
-
+    asyncTest('Page load deferred test', function() {
+        var page = Page(undefined, {disableHintingEvents: true});
         page.load.done(function() {
             ok(page);
-            start();
         });
+        start();
     });
 
-    test('Page height based element creation', function() {
-        var args = [{text: 'some text'}];
-        var page = Page(undefined, {template: ArgTmpl(args)});
-        ok(page);
+    /***** ADD OPTIONS EVENT BASED ENABLEMENT OF FADE QUEUE ******/
+    /* Testing utils */
+    var ensureHintThreshold = 3000;
+    var showHintTemplate = "<div style='display:block; position:absolute; top:0; height:" + ensureHintThreshold + "'/></div>"; // This is not working
+    var fadeSelector = 'div.more-document-hinting';
 
-        var bigDocumentHeight = 1200;
-        page.load.done('loaded', function() {
-            $('body').css('height', bigDocumentHeight);
-            var display = $('div.more-document-hinting').css('display');
-
-            equal(display, 'block', 'Body height set to 1200, hinting should be visible');
-
-            stop(); // Wait for the fadeOut to complete before checking disappearance.
-            $(document).ready(function() {
-                $(window).scrollTop(bigDocumentHeight);
+    var fadeDelayTest = function(tests) {
+        /*Tests will take the form {behavior: b, eventSimulation: e, method: m)}*/
+        var hintPage = Page(undefined, {template: showHintTemplate, 
+                                        disableHintingEvents: true});
+        hintPage.load.done( function() {
+            _.each(tests, function(test, index) {
+                var testHintHeight = 60;
+                // Trigger event handler as if user viewport is far from bottom
+                hintPage[test.behavior](test.eventSimulation, 
+                                        testHintHeight,
+                                        hintPage.options.displayHintThreshold+30);
                 setTimeout(function() {
-                    display = $('div.more-document-hinting').css('display');
-                    equal(display, 'none',
-                        'Programmatically scrolled to the bottom, hint should disappear');
+                    test.method(hintPage);
                     start();
-                },1000);
+                }, (hintPage.options.fadeRate - hintPage.options.fadeRate/2));
             });
         });
+    };
+    var genTest = function(behavior, eventSimulation, method) {
+        return {behavior: behavior, eventSimulation: eventSimulation, method: method};
+    };
+    asyncTest('fade initially shown', function() {
+        expect(1);
+        var testOne = genTest('_initFooterShadow', undefined, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'in',
+                  'page initialized with height greater than hint threshold');
+        });
+        fadeDelayTest([testOne]);
+    });
+
+    /* shadow hinting tests */
+    asyncTest('_mouseLeaveBehavior', function() {
+        var testOne = genTest('_mouseLeaveBehavior', undefined, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'in',
+                  "on mouse leave the fader should be present");
+        });
+        fadeDelayTest([testOne]);
+    });
+    asyncTest('_mouseEnterBehavior', function() {
+        var e = {pageY: $(window).scrollTop()+$(window).height()-5};
+        var testOne = genTest('_mouseEnterBehavior', e, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'out',
+                  "on mouse enter the fader should not be present on hover");
+        });
+        fadeDelayTest([testOne]);
+    });
+
+    asyncTest('_mouseMoveBehavior', function() {
+        var e = {pageY: $(window).scrollTop()+$(window).height()-5};
+        var testOne = genTest('_mouseMoveBehavior', e, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'out',
+                  'mouse move triggers minimum opacity only if mouse is over the fade');
+        });
+        fadeDelayTest([testOne]);
+    });
+    asyncTest('_clickBehavior', function() {
+        var eOnHint = {pageY: $(window).scrollTop()+$(window).height()-5};
+        var testOne = genTest('_clickBehavior', eOnHint, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'out',
+                  'mouse move triggers minimum opacity if mouse is on the fade');
+        });
+        var eOffHint = {pageY: 0};
+        var testTwo = genTest('_clickBehavior', eOffHint, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'in',
+                  'mouse click triggers maximum opacity if mouse is off the fade');
+        });
+        fadeDelayTest([testOne]);
+    });
+    asyncTest('_pollViewPos', function() {
+        var e = {pageY: $(window).scrollTop()+$(window).height()-5};
+        var testOne = genTest('_pollViewPos', e, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'in',
+                  '_pollViewPos triggers does nothing if mouse is not over fade');
+        });
+        fadeDelayTest([testOne]);
+    });
+    asyncTest('_initFooterShadow', function() {
+        var e = {pageY: $(window).scrollTop()+$(window).height()-5};
+        var testOne = genTest('_initFooterShadow', e, function(hintPage) {
+            equal(hintPage.fadeQueue.getCurrentDirection(), 'in',
+                  'init footer shadow should defaut to "in"');
+        });
+        fadeDelayTest([testOne]);
     });
     start();
 });
