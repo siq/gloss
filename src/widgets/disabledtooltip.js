cb48fe8072ee33e23aab7c6e813149ca5747d23d
Brigit Foster <Foster.Brigit@gmail.com>
2013-10-01 17:45:55 -0500

    DAQ-728 - Syntax cleanup

--- src/widgets/disabledtooltip.js
+++ src/widgets/disabledtooltip.js
@@ -13,38 +13,31 @@ define([
         create: function() {
             var self = this;
             this._super();
-            var $overlay = $('<div style="position:absolute;left:0;right:0;top:0;bottom:0;"></div>')
+            var $overlay = $('<div style="position:absolute;left:0;right:0;top:0;bottom:0;"></div>');
             this.options.wrapTarget.wrap($('<div style="display:inline-block;position:relative;" />'));
             this.options.wrapTarget.after($overlay);
             this.set('target', $overlay);
-            $overlay.on( "hover", function() {
-                if (self.isWrapTargetDisabled())
-                {
-                    $overlay.css( "cursor","default");
-                }
-                else
-                {
-                    $overlay.css( "cursor","pointer");
+            $overlay.on('hover', function() {
+                if (self.isWrapTargetDisabled()) {
+                    $overlay.css('cursor', 'default');
+                } else {
+                    $overlay.css('cursor', 'pointer');
                 }
             });
-            $overlay.on( "click", function() {
-                self.options.wrapTarget.trigger( "click" );
+            $overlay.on('click', function() {
+                self.options.wrapTarget.trigger('click');
             });
             this.update();
         },
         _initiateShow: function(event) {
-            if (this.isWrapTargetDisabled())
-            {
+            if (this.isWrapTargetDisabled()) {
                 this._super(event);
             }
         },
         isWrapTargetDisabled: function() {
-            if (this.options.wrapTarget.is(":disabled") || this.options.wrapTarget.hasClass("disable"))
-            {
+            if (this.options.wrapTarget.is(':disabled') || this.options.wrapTarget.hasClass('disable')) {
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
         }
