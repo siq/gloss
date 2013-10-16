ee26f9348ea8b0a45c71426d53f2916cbba4bd62
cad <cad@yggdrasil-M2.(none)>
2013-10-16 13:11:46 -0500

    UX-78: open time select on tab/focus in, close on tab out

--- src/widgets/timeselect.js
+++ src/widgets/timeselect.js
@@ -1,17 +1,17 @@
 define([
+    './widget',
     './simpleview',
     './timeselect/private',
     'css!./timeselect/timeselect.css'
-], function(SimpleView, priv) {
-
-    var TimeSelect = SimpleView.extend({
+], function(Widget, SimpleView, priv) {
+    return SimpleView.extend({
         defaults: {
             format: 'hh:mm A',
             hoverColor: '#ccc',
             minuteIncrement: 30
         },
         template: '<div class=time-select><input class=time-input type=text></input><div class="time-drop"></div></div>',
-
+        
         _initWidgets: function() {
             var self = this;
             this._super.apply(this, arguments);
@@ -27,8 +27,8 @@ define([
             $input = self.$el.find('.time-input');
             self._super.apply(this,arguments),
 
-            $input.click(function(){
-                self.timeSelector.find('.time-options').toggleClass('hidden');
+            $input.on('focus click', function(){
+                self.timeSelector.find('.time-options').removeClass('hidden');
                 priv.set_time_elements_to_current_time(self.timeSelector,
                                                        self.$timeElements,
                                                        self.get('minuteIncrement'),
@@ -38,11 +38,14 @@ define([
                 $input.val(evt.target.innerHTML);
                 self.timeSelector.find('.time-options').toggleClass('hidden');
             });
-            return self;
+            $input.on('keydown', function(evt) {
+                if (Widget.identifyKeyEvent(evt) === 'tab') {
+                    self.timeSelector.find('.time-options').addClass('hidden');
+                }
+            });
         },
         getValue: function() {
            return priv.parse_time(this.$el.find('.time-input').val(), false);
         }
     });
-    return TimeSelect;
 });
