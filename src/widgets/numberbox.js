54c25457effb9ed3e4e23ba7967224c2d2294db3
cdablain <cad.yggdrasil@gmail.com>
2013-10-29 11:15:38 -0500

    PIQ-4: added numberbox widget functionality for html 5 style validation across browsers. (scheduler monthly/weekly frequency widgets)

--- src/widgets/numberbox.js
+++ src/widgets/numberbox.js
@@ -1,37 +1,73 @@
 define([
     'vendor/jquery',
-    './textbox'
-], function($, TextBox) {
-    return TextBox.extend({
+    'vendor/underscore',
+    './formwidget'
+], function($, _, FormWidget) {
+    /* Utilities to push the post-decimal values of a floating point number into the integer component of a float.
+     * Prevents floating point errors on a modulo operation */
+    var priv = new function() {
+        this._read_precision = function(num) {
+            var numStr = String(num),
+                index = numStr.indexOf('.');
+            return (!isNaN(num) && index !== -1) ? (numStr.split('.')[1].length) : 0;
+        };
+        this._destroy_mantissa = function(num) {
+            var prec = this._read_precision(num);
+            return (prec===-1 || prec===0) ? num : num * Math.pow(10, prec);
+        };
+        this._mod_float = function(num, step) {
+            var prec = this._read_precision(num),
+                sprec = this._read_precision(step),
+                tNum = this._destroy_mantissa(num) * Math.pow(10, sprec),
+                tStep = this._destroy_mantissa(step) * Math.pow(10, prec),
+                numerator = tNum%tStep,
+                denominator = prec * sprec;
+            return (denominator===0) ? numerator : numerator / denominator;
+        };
+        this._step_is_less_precise_than_input = function(num, step) {
+            return (this._read_precision(num) > this._read_precision(step));
+        };
+        /* The assumption here is that if a step is provided, that step represents the lowest allowed unit of discriminiation
+         * The mod float logic does not work for steps that are less precise than the input value */
+        this._passes_constraints = function(num, constraints) {
+            return ((!isNaN(num) && isFinite(num)) &&
+                    ((constraints.min) ? (num >= constraints.min) : true) &&
+                    ((constraints.max) ? (num <= constraints.max) : true) &&
+                    ((constraints.step) ? (!this._step_is_less_precise_than_input(num, constraints.step)) : true) &&
+                    ((constraints.step) ? (this._mod_float(num, constraints.step) === 0) : true));
+        };
+    };
+    /**
+     * This widget only supports numbers with an absolute value less than 9007199254740992 when the number's decimal point is removed,
+     * and when the precision of a specified step is added to the precision of the input field.
+     * Example - value: 10.001 with a step: 0.000001 is represented as 10001000000 during step validation.
+     */
+    var NumberBox = FormWidget.extend({
         nodeTemplate: '<input type=number>',
         defaults: {
-            restriction: null,
             placeholder: null
         },
-
         create: function() {
-            var restriction, 
-                min = this.$node.attr('min'),
-                step = this.$node.attr('step');
-            
-            if (this.options.restriction == null) {
-                if (step != null && parseInt(step, 10) % 1 === 0) {
-                    restriction += 'integer';
-                }
+            this._super.apply(this, arguments);
+            this.options.min = this.$node.attr('min'),
+            this.options.max = this.$node.attr('max'),
+            this.options.step = this.$node.attr('step');
 
-                if (min != null && parseInt(min, 10) === 0) {
-                    restriction += ' positive';
-                }
-
-                this.options.restriction = $.trim(restriction) || 'float';
-            }
-
-            this._super();
         },
-
+        getContraints: function() {
+            return {min: this.options.min, max: this.options.max, step: this.options.step};
+        },
+        setValue: function(val) {
+            if(priv._passes_constraints(Number(val), this.getContraints()) || val=== '') {
+                this._super.apply(this, arguments);
+            }
+        },
         getValue: function() {
             var value = this._super.apply(this, arguments);
-            return value !== '' ? value : null;
+            return (value !== '') ? value : null;
         }
     });
+    return function(opts, withPrivate) {
+        return (withPrivate) ? _.extend(NumberBox.extend(opts)(), priv) : NumberBox.extend(opts)();
+    };
 });
--- src/widgets/numberbox/test.js
+++ src/widgets/numberbox/test.js
@@ -3,23 +3,93 @@ define([
     'vendor/jquery',
     './../numberbox'
 ], function($, NumberBox) {
-
+    var numberBox = NumberBox({nodeTemplate: '<input type=number min=-10 max=1000 step=0.0001></input>'}, true);
     test('numberbox instantiation', function() {
-        ok(NumberBox().appendTo('#qunit-fixture'));
+        ok(numberBox.prependTo('body'));
     });
 
+    test('_read_precision', function() {
+        equal(numberBox._read_precision(10.00), 0);
+        equal(numberBox._read_precision(10.001), 3);
+        equal(numberBox._read_precision(10.0001), 4);
+        equal(numberBox._read_precision(10), 0);
+        equal(numberBox._read_precision(1), 0);
+    });
+    test('_destroy_mantissa', function() {
+        equal(numberBox._destroy_mantissa(.001), 1);
+        equal(numberBox._destroy_mantissa(0.001), 1);
+        equal(numberBox._destroy_mantissa(10.01), 1001);
+        equal(numberBox._destroy_mantissa(30.003), 30003);
+    });
+    test('_mod_float', function() {
+        equal(numberBox._mod_float(10.01, 0.01), 0);
+        equal(numberBox._mod_float(1.01, 0.01), 0);
+        equal(numberBox._mod_float(10.02, 0.02), 0);
+        equal(numberBox._mod_float(10.02, 0.03), 0);
+        equal(numberBox._mod_float(10.02, 0.001), 0);
+        equal(numberBox._mod_float(713.02, 0.02), 0);
+    });
+    test('_step_is_less_precise_than_input', function() {
+        ok(numberBox._step_is_less_precise_than_input(10.001, 0.5));
+        ok(!numberBox._step_is_less_precise_than_input(10.01, 0.005));
+    });
+    test('_passed_contraints', function() {
+        ok(numberBox._passes_constraints(1.01, {
+            min: 0, max: 2, step: 0.01
+        }));
+        ok(!numberBox._passes_constraints(1.01, {
+            min: 2, max: 10, step: 0.01
+        }));
+        ok(!numberBox._passes_constraints(1.01, {
+            min: -10, max: 1, step: 0.01
+        }));
+        ok(!numberBox._passes_constraints(1.01, {
+            min: 0, max: 2, step: 0.1
+        }));
+    });
     test('numberbox blank value', function() {
-        var numberBox = NumberBox();
-
-        equal(numberBox.getValue(), null);
-
-        numberBox.setValue('123');
-        equal(numberBox.getValue(), '123');
-
         numberBox.setValue('');
         equal(numberBox.getValue(), null);
-
+        numberBox.setValue('123');
+        equal(numberBox.getValue(), '123');
+        numberBox.setValue('');
+        equal(numberBox.getValue(), null);
     });
 
+    test('disallowed alphabetics', function() {
+        numberBox.setValue('');
+        numberBox.setValue('abc');
+        equal(numberBox.getValue(), null);
+    });
+    test('min number contraint', function() {
+        // Min specified at construction is -10
+        numberBox.setValue(-10);
+        equal(numberBox.getValue(), -10);
+        numberBox.setValue(-100);
+        equal(numberBox.getValue(), -10); // Last valid value
+        numberBox.setValue(-11);
+        equal(numberBox.getValue(), -10); // Last valid value
+    });
+    test('max number contraint', function() {
+        // Max specified at construction is 1000
+        numberBox.setValue(10);
+        equal(numberBox.getValue(), 10);
+        numberBox.setValue(1000);
+        equal(numberBox.getValue(), 1000);
+        numberBox.setValue(1001);
+        equal(numberBox.getValue(), 1000); // Last valid value
+
+    });
+    test('step contraint', function() {
+        // Step specified at construction is 0.0001
+        numberBox.setValue(1);
+        equal(numberBox.getValue(), 1);
+        numberBox.setValue(1.00001);
+        equal(numberBox.getValue(), 1); // Last valid value
+        numberBox.setValue(1.002);
+        equal(numberBox.getValue(), 1.002);
+        numberBox.setValue(999.00005);
+        equal(numberBox.getValue(), 1.002); // Last valid valud
+    });
     start();
 });
