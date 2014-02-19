e0f9ab089186d5c5bdc3ada292f1260a0b4f1eb7
paramv <parameshv.29@gmail.com>
2014-02-19 18:55:35 +0530

    UI-9 updated tests to run scroll to top and bottom as separate test cases

--- src/widgets/user_display/powergrid/infinitescrolltest.js
+++ src/widgets/user_display/powergrid/infinitescrolltest.js
@@ -2,8 +2,9 @@
   raises, getTopBottom*/
 
 
-
-getTopBottom = function($,el){
+// Utility function to get the current value of first and last item, total items in the list
+var getTopBottom = function($,el){
+    /*lazy function definition*/
 	getTopBottom = function(dontPrint){
 		var rows = el.find('tr'),
 			rowLen = rows.length,first,last;
@@ -14,17 +15,13 @@ getTopBottom = function($,el){
 		}
 		return {first:first,last:last,total:rowLen-1}
 	}
-	getTopBottom();
-	return getTopBottom;
+	return getTopBottom();
 	
-}
+};
 define([
     'vendor/jquery',
     'vendor/underscore',
-    'vendor/moment',
-    './../../../util/scrollbar',
     './../powergrid',
-    './columnmodel',
     './column',
     './column/checkboxcolumn',
     './column/asdatetime',
@@ -34,93 +31,102 @@ define([
     'mesh/tests/mockedexample',
     'mesh/tests/examplefixtures',
     './utils'
-], function($, _, moment, scrollbar, PowerGrid, ColumnModel, Column,
+], function($, _, PowerGrid, Column,
     CheckBoxColumn, asDateTime, asBytes, asNumber, asFormWidget, Example,
     exampleFixtures, utils) {
 
 	var totalCount = 1000;
-    var BasicColumnModel = utils.BasicColumnModel,
-        setup = utils.setup,
-        trim = utils.trim;
+    var setup = utils.setup,
+        trim = utils.trim,
+        $dfd = $.Deferred(),
+        scrollToBottom,
+        scrollToTop,
+        scrollToEnd,
+        scrollToBeginning,
+        testyDown = $('<button>Scroll To Bottom</button>'),
+        testyUp = $('<button>Scroll To Top</button>');
 
-    function aboutEqual(a, b, msg, tolerance) {
-        var greater = a > b? a : b;
-        msg = msg == null? 'values are within ' + tolerance*100 + '%' : msg;
-        tolerance = tolerance || 0.02;
-        ok(Math.abs(a-b) / greater < tolerance, msg);
-    }
 	module('infinite scroll');
-
       
-    asyncTest('scroll to either end of records works', function() {
+    $dfd.then(function(){console.log('then')});
+    asyncTest('scroll to end of records works', function() {
         setup({
-            // appendTo: 'body',
-            params: {limit: 200},
+            params: {limit: 100},
             gridOptions: {
                 infiniteScroll: true,
-                increment: 100,
+                increment: 400,
                 keyboardNavigation:false,
-                bufferSize:150
+                bufferSize:500
             },
             delay:300,
             appendTo: 'body'
         }).then(function(g, options) {
-            // set height and widths for visual resize testing
-            g.$el.height(400);
-            g.$el.width(800);
-            // rerender so the height and width changes are pickued up
-            g.rerender();
-			
             var collection = g.get('collection'),
             	incr = g.get('increment'),
             	oldLimit = collection.query.params.limit,
             	$rowWrapper = g.$el.find('.row-inner-wrapper'),
                 $rows = g.$el.find('.rows'),
                 buffer = g.get('bufferSize'),
-                scrollToEnd,
-                scrollToBeginning,
-                inter,
-                count = 0,
-                testyDown = $('<button>Scroll To Bottom</button>'),
-                testyUp = $('<button>Scroll To Top</button>'),
-                scrollToBottom = function(e){
-		        	$rowWrapper.scrollTop($rows.height());
-		        	//setTimeout(getTopBottom,500);
-		        },
-		        scrollToTop = function(e){
-		        	$rowWrapper.scrollTop(0);
-		        	//setTimeout(getTopBottom,500);
-		        };
-			
-			$('body').append(testyDown).append(testyUp); 
-            testyDown.on('click',scrollToBottom);
-			testyUp.on('click',scrollToTop);
-			
-			getTopBottom($,$rowWrapper);
+                inter;
+            
+            // set height and widths for visual resize testing
+            g.$el.height(400);
+            g.$el.width(800);
+            // rerender so the height and width changes are pickued up
+            g.rerender();
+			    
+            scrollToBottom = function(e){
+		        $rowWrapper.scrollTop($rows.height());
+		      	//setTimeout(getTopBottom,500);
+		    };
+		    scrollToTop = function(e){
+		        $rowWrapper.scrollTop(0);
+		       	//setTimeout(getTopBottom,500);
+		    };
 			
 			scrollToBeginning = function(){
-			    if(getTopBottom(true).first === "item 0"){
-			        clearTimeout(inter);
-			        scrollToTop();
-			        ok(true,"Reached beginning of list");
-			        start();
-			        return;
-			    }
-			    scrollToTop();
-			    inter = setTimeout(scrollToBeginning,700);
-			}
-			scrollToEnd = function(){
+		        if(getTopBottom(true).first === "item 0"){
+		            clearTimeout(inter);
+		            scrollToTop();
+		            ok(true,"Reached beginning of list");
+		            start();
+		            return;
+		        }
+		        scrollToTop();
+		        inter = setTimeout(scrollToBeginning,700);
+		    };
+		    
+		    scrollToEnd = function(){
 				if(g._isAllDataLoaded()){
 					clearTimeout(inter);
 					scrollToBottom();
 					ok(true,"Reached end of list");
-					inter = setTimeout(scrollToBeginning,600);
+					ok(true,"scroll to end completed, kicking off scroll to beginning now");
+					start();
+					$dfd.resolve();
 					return;
 				}
 				$rowWrapper.scrollTop($rows.height());
 				inter = setTimeout(scrollToEnd,700);
-			}
+			};
+			
+			// Utility buttons for one click scroll up/down	, uncomment for visual testing		
+			
+			/*$('body').append(testyDown).append(testyUp); 
+            testyDown.on('click',scrollToBottom);
+			testyUp.on('click',scrollToTop);*/
+			
+			// Get the initial value of first, last and total
+			getTopBottom($,$rowWrapper);
+			
+			// Begin scroll to end of the list
             scrollToEnd();
+        });
+    });
+    asyncTest('scroll to beginning of records works', function() {	
+        $dfd.then(function(){
+            // Begin scroll to top of the list
+            setTimeout(scrollToBeginning,1200);
         });
     });
     start();
