a988d9cdcfc85430d3e1f2c58a0d0cbfa33d78bf
Brigit Foster <bfoster@us.ibm.com>
2015-03-04 09:47:25 -0600

    Defect 6265: Class added to disabledtooltip div wrappers for IE9 styling fix.

--- src/widgets/user_display/disabledtooltip.js
+++ src/widgets/user_display/disabledtooltip.js
@@ -14,8 +14,8 @@ define([
         create: function() {
             var self = this;
             this._super();
-            var $overlay = $('<div style="position:absolute;left:0;right:0;top:0;bottom:0;"></div>');
-            this.options.wrapTarget.wrap($('<div style="display:inline-block;position:relative;" />'));
+            var $overlay = $('<div class="overlay" style="position:absolute;left:0;right:0;top:0;bottom:0;"></div>');
+            this.options.wrapTarget.wrap($('<div class="wraptarget" style="display:inline-block;position:relative;" />'));
             this.options.wrapTarget.after($overlay);
             this.set('target', $overlay);
             $overlay.on('hover', function() {
