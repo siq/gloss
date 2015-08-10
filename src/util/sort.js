d4df853a02953595a258301b853da146b32a8a9e
Ralph Smith <agentsmith@us.ibm.com>
2015-08-10 14:56:10 -0500

    defect 6694 add normalized ip address sorting

--- src/util/sort.js
+++ src/util/sort.js
@@ -4,17 +4,27 @@ define([
     'use strict';
 
     var normalize = function(text) {
-        var normalizedTxt = text.toString().toLowerCase()
-            .replace(/^\W+/, '').replace(/\W+$/, '');
-        if (!normalizedTxt) {
-            // if there is no text after normalization then we can't normalize
-            // using the reverse 'word character' regex (\W)
-            // so just return the original text
-            // this can occure with international characters like Chinese
-            return text;
-        }
-        return normalizedTxt;
-    };
+            var normalizedTxt = text.toString().toLowerCase()
+                .replace(/^\W+/, '').replace(/\W+$/, '');
+            if (!normalizedTxt) {
+                // if there is no text after normalization then we can't normalize
+                // using the reverse 'word character' regex (\W)
+                // so just return the original text
+                // this can occure with international characters like Chinese
+                return text;
+            }
+            return normalizedTxt;
+        },
+        isIpAddress = function(text) {
+            if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(text)) {
+                return true;
+            }
+            return false;
+        },
+        normalizeIp = function(ipAddress) {
+            var v = ipAddress.split(".");
+            return v[0]*0x1000000 + v[1]*0x10000 + v[2]*0x100 + v[3]*1;
+        };
 
     return {
         // this function is a comparator intended to be passed to Array#sort()
@@ -46,6 +56,10 @@ define([
                 !(_.isNumber(a) && _.isNumber(b))) {
                 a = normalize(a);
                 b = normalize(b);
+            }
+            if (isIpAddress(a) && isIpAddress(b)) {
+                a = normalizeIp(a);
+                b = normalizeIp(b);
             }
             return ((a < b) ? -1 : ((a > b) ? 1 : 0));
         }
