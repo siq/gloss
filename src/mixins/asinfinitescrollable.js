4b4f873772fc215ea919dab14f5ac4ce82d9d703
Ralph Smith <agentsmith@us.ibm.com>
2016-02-11 12:40:32 -0700

    allow delegation for scrolling el (works with powergrid)
    in the future powergrid will use this but until then the powergrid
    usage would look like this
    asInfiniteScrollable.call(PowerGrid.prototype, {
            delegate: '.row-inner-wrapper',
            render: '_rerender',
            limit: 50,
        });

--- src/mixins/asinfinitescrollable.js
+++ src/mixins/asinfinitescrollable.js
@@ -21,7 +21,7 @@ define([
     //             |               |                           |
     //         ____|_______________|____   <---|               |
     //         |   |               |   |       |               |
-    //         |   |               |   |   list height     Window Size
+    //         |   |               |   |   list height     Window Size = 2*limit
     //         |   |               |   |       |               |
     //         |___|_______________|___|   <---|               |
     //             |               |                           |
@@ -31,31 +31,38 @@ define([
         this.defaults = $.extend({}, this.defaults, {
             offset: 0,
             limit: 25,
+            delegate: null,
+            render: 'render',
             scrollFunction: function() {
                 return 0;
             },
         }, options);
 
-        this._bindEvents = _.wrap(this._bindEvents, function(_bindEvents) {
+        this.init = _.wrap(this.init, function(init) {
+            var ret = init.apply(this, _.rest(arguments, 1));
             this._bindInfiniteScroll();
-            return _bindEvents.apply(this, _.rest(arguments, 1));
+            return ret;
         });
         this._bindInfiniteScroll = function() {
-            var self = this,
+            var self     = this,
+                render   = this.get('render'),
+                delegate = this.get('delegate'),
+                $el      = delegate ? this.$el.find(delegate) : this.$el,
+                el       = $el[0],
                 scrollingDown = function() {
-                    return (this.el.scrollHeight-(this.$el.height()*2))/2;
+                    return (el.scrollHeight-($el.height()*2))/2;
                 },
                 scrollingUp = function() {
-                    return this.el.scrollHeight/2;
+                    return el.scrollHeight/2;
                 };
 
-            this.on('scroll', function() {
+            $el.on('scroll', function() {
                 var collection,
                     limit   = self.get('limit'),
                     offset  = self.get('offset'),
-                    top     = self.el.scrollTop === 0,
-                    bottom  = self.el.scrollHeight - self.el.scrollTop === self.el.clientHeight ||
-                        self.el.scrollHeight - self.el.scrollTop === self.el.clientHeight + self.el.clientTop,
+                    top     = el.scrollTop === 0,
+                    bottom  = el.scrollHeight - el.scrollTop === el.clientHeight ||
+                        el.scrollHeight - el.scrollTop === el.clientHeight + el.clientTop,
                     isAllDataLoaded     = self._isAllDataLoaded(),
                     isFirstWindowLoaded = self._isFirstWindowLoaded(),
                     isLastWindowLoaded  = self._isLastWindowLoaded(),
@@ -67,8 +74,7 @@ define([
                     // offset can't be less than 0
                     self.set('offset', Math.max(0, offset-limit));
                     self.set('scrollFunction', scrollingUp);
-                    self.render();
-                    // self.$el.scrollTop(self.el.scrollHeight/2);
+                    self[render]();
                 } else if (top && isFirstWindowLoaded) {
                     // console.log('hit top');
                     return;
@@ -85,7 +91,6 @@ define([
                     // isAllDataLoaded = false indicates there is more to come from the server
                     // isLastWindowLoaded = true; indicates we have scrolled out all the local models
                     // and need to fetch new models from the server
-
                     collection = self.get('collection');
                     if (!collection) {
                         console.warn('no collection to load');
@@ -105,65 +110,81 @@ define([
                     limit = self.get('limit');
                     self.set('offset', offset+limit);
                     self.set('scrollFunction', scrollingDown);
-                    self.render();
+                    self[render]();
                 }
             });
         };
-        this._getTotalModelCount = function() {
-            return this.get('collection.total') || this.get('data.length') || 0;
-        };
-        this._getWindowedModels = function() {
-            var models = this.get('models') || [],
-                offset = this.get('offset'),
-                limit = this.get('limit'),
-                total = this._getTotalModelCount(),
-                startIndex,
-                endIndex;
+        if (!this._getTotalModelCount) {
+            this._getTotalModelCount = function() {
+                return this.get('collection.total') || this.get('data.length') || 0;
+            };
+        }
+        if (!this._getWindowedModels) {
+            this._getWindowedModels = function() {
+                var models = this.get('models') || [],
+                    offset = this.get('offset'),
+                    limit = this.get('limit'),
+                    total = this._getTotalModelCount(),
+                    startIndex,
+                    endIndex;
 
-            // the offset can't be greater than the total-1imit
-            offset = Math.min(offset, total-limit);
-            // create a window that is 2*limit
-            startIndex = Math.max(0, offset-limit);
-            endIndex = startIndex === 0 ? limit*2 : offset+limit;
-            // console.log('startIndex:', startIndex);
-            // console.log('endIndex:', endIndex);
-            return models.slice(startIndex, endIndex);
-        };
-        this._isAllDataLoaded = function() {
-            var models = this.get('models') || [];
-            return this._getTotalModelCount() === models.length;
-        };
-        this._isFirstWindowLoaded = function() {
-            var offset = this.get('offset'),
-                limit = this.get('limit'),
-                currentWindow = Math.floor(Math.max(0, offset-limit)/limit) + 1;
-            return currentWindow === 1;
-        };
-        this._isLastWindowLoaded = function() {
-            var offset = this.get('offset'),
-                limit = this.get('limit'),
-                // current total of models loaded
-                total = (this.get('models') || []).length,
-                windowCount = Math.ceil(total/limit) || 1,
-                currentWindow = Math.floor(offset/limit) + 1;
-            return windowCount === currentWindow;
-        };
+                // the offset can't be greater than the total-1imit
+                offset = Math.min(offset, total-limit);
+                // create a window that is 2*limit
+                startIndex = Math.max(0, offset-limit);
+                endIndex = startIndex === 0 ? limit*2 : offset+limit;
+                // console.log('startIndex:', startIndex);
+                // console.log('endIndex:', endIndex);
+                return models.slice(startIndex, endIndex);
+            };
+        }
+        if (!this._isAllDataLoaded) {
+            this._isAllDataLoaded = function() {
+                var models = this.get('models') || [];
+                return this._getTotalModelCount() === models.length;
+            };
+        }
+        if (!this._isFirstWindowLoaded) {
+            this._isFirstWindowLoaded = function() {
+                var offset = this.get('offset'),
+                    limit = this.get('limit'),
+                    currentWindow = Math.floor(Math.max(0, offset-limit)/limit) + 1;
+                return currentWindow === 1;
+            };
+        }
+        if (!this._isLastWindowLoaded) {
+            this._isLastWindowLoaded = function() {
+                var offset = this.get('offset'),
+                    limit = this.get('limit'),
+                    // current total of models loaded
+                    total = (this.get('models') || []).length,
+                    windowCount = Math.ceil(total/limit) || 1,
+                    currentWindow = Math.floor(offset/limit) + 1;
+                return windowCount === currentWindow;
+            };
+        }
         // This is a utility function to scroll to an element within a scrollable view
         // It is not used by the base logic
-        this._scrollTo = function($el, offsetTop) {
-            var scrollY;
-            offsetTop = _.isNumber(offsetTop) ? offsetTop : 250;
-            // offsetTop = $el.height();
-            scrollY = $el.offset().top + this.$el.scrollTop() - offsetTop;
-            this.$el.scrollTop(scrollY);
-            console.log(offsetTop);
-            return this;
-        };
-        this.render = _.wrap(this.render, function(render) {
+        if (!this._scrollTo) {
+            this._scrollTo = function($el, offsetTop) {
+                var scrollY;
+                offsetTop = _.isNumber(offsetTop) ? offsetTop : 250;
+                // offsetTop = $el.height();
+                scrollY = $el.offset().top + this.$el.scrollTop() - offsetTop;
+                this.$el.scrollTop(scrollY);
+                // console.log(offsetTop);
+                return this;
+            };
+        }
+        this[this.defaults.render] = _.wrap(this[this.defaults.render], function(render) {
             var scrollFunction = this.get('scrollFunction'),
-                models = this._getWindowedModels();
+                models   = this._getWindowedModels(),
+                delegate = this.get('delegate'),
+                $el;
             render.call(this, models);
-            this.$el.scrollTop(scrollFunction.apply(this));
+            // render might wipeout the dom el if we find it first so we do it after
+            $el = delegate ? this.$el.find(delegate) : this.$el;
+            $el.scrollTop(scrollFunction.apply(this));
             return this;
         });
     }
