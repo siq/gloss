363bb49ccb6b30e9319be5f3a37a22ce61c76e4b
Ralph Smith <agentsmith@us.ibm.com>
2016-04-07 15:44:50 -0600

    add back drop to offcanvas, hook up esc button for specific canvas

--- src/views/offcanvas.js
+++ src/views/offcanvas.js
@@ -10,6 +10,7 @@ define([
         defaults: {
             title: null,
             closeBtn: true, // 'true' to have a close button in the upper corner
+            backdrop: true,
             templates: {content: ''},
         },
         template: template,
@@ -17,13 +18,33 @@ define([
         _bindEvents: function() {
             var self = this;
 
-            _.bindAll(this, 'hide');
+            _.bindAll(this, 'hide', 'close');
             // close on escape
-            $(document).on('keyup', function(evt) {
-                if (evt.which === 27) {
+            // $(document).on('keyup', function(evt) {
+            //     if (evt.which === 27) {
+            //         self.hide();
+            //     }
+            // });
+            $(document).on('keyup', '.off-canvas', function(evt) {
+                var thisCavnas = self.el === evt.currentTarget;
+                if (evt.which === 27 && thisCavnas) {
                     self.hide();
                 }
             });
+            this.on('transitionend', function(evt) {
+                console.log('transitionend');
+                var $el = $(evt.target),
+                    marginRight = $el.css('margin-right'),
+                    visible = marginRight === '0px';
+                if (visible) {
+                    // self._focusOnFirstVisibleBinding();
+                    // set focus on first focusable element
+                    var _$el = $el.find('a, button, :input, [tabindex]')
+                        .filter(':visible:not(":disabled"):first').focus();
+                }
+            });
+            this.$backdrop.on('click', this.close);
+
             // close on of canvas click
             // TODO: the off click doens't play well when a powergrid is in view
             // .on('mouseup', function(evt) {
@@ -36,10 +57,37 @@ define([
                 .on('click', 'h1 > .close-button', this.hide);
             return this._super.apply(this, arguments);
         },
+        _focusOnFirstVisibleBinding: function() {
+            var widgets = _.mpluck(this._visibleBindings(), 'widget'),
+                widget = _.first(widgets),
+                $el = widget && (widget.$node || widget.$el);
+            if ($el && $el.is(':visible') && !$el.is(':disabled')) {
+                // stupid time out for IE8 bug
+                setTimeout(function() {$el.focus();}, 10);
+            }
+        },
+        _initWidgets: function() {
+            var self = this;
+            this._super.apply(this, arguments);
+            this.$backdrop = this.get('backdrop') ?
+                $('<div class="off-canvas-backdrop hidden"></div>') : $(null);
+            return this;
+        },
+        _visibleBindings: function() {
+            var bindings = this.bindingGroup && this.bindingGroup.bindings;
+            return _.filter(bindings, function(binding) {
+                var $el = binding.has('widget')?
+                    binding.get('widget.$node') || binding.get('widget.$el') :
+                    binding.get('$el');
+                return $el.is(':visible');
+            });
+        },
         close: function() {
             return this.hide.apply(this, arguments);
         },
         hide: function() {
+            this.$el.blur();
+            this.$backdrop.addClass('hidden');
             this._super.apply(this, arguments);
             this.trigger('hide');
         },
@@ -47,6 +95,10 @@ define([
             return this.show.apply(this, arguments);
         },
         show: function() {
+            if (!this.$backdrop.parent().length && this.$el.parent().length) {
+                this.$backdrop.insertBefore(this.$el);
+            }
+            this.$backdrop.removeClass('hidden');
             this._super.apply(this, arguments);
             this.trigger('show');
         }
