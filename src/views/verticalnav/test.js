1466d5eb75334e8fd1e0b22700e705b63f4bebad
Ralph Smith <agentsmith@us.ibm.com>
2016-06-10 06:54:01 -0600

    fix-up vertical nav tabbing

--- src/views/verticalnav/test.js
+++ src/views/verticalnav/test.js
@@ -7,107 +7,124 @@ define([
     var VerticalNavTest = VerticalNav.extend({
         template: testTmpl,
     });
-    test ('instantiation', function(){
-        var bc = VerticalNavTest()
+    test ('declarative template', function(){
+        var bc = VerticalNavTest({
+                template: testTmpl,
+            })
+            .appendTo('#qunit-fixture');
+            // .appendTo('body');
+        ok(bc);
+        $('[id^=qunit-]').hide();
+        $('body').css('margin', 0);
+    });
+
+    test ('defaults configuration', function(){
+        var bc = VerticalNav({
+                items: [
+                    {
+                        content: 'Dashboard',
+                        value: '/',
+                        icon: './img/catalog.svg',
+                    },
+                    {
+                        content: 'Details',
+                        value: '/details',
+                        icon: './img/catalog.svg',
+                        children: [
+                            {
+                                content: 'Ancestry',
+                                value: '/details/ancestry',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'Action Log',
+                                value: '/details/action-log',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'Data Objects',
+                                value: '/details/sample-objects',
+                                icon: './img/catalog.svg',
+                            },
+                        ],
+                    },
+                    {
+                        content: 'Refine',
+                        value: '/explore',
+                        icon: './img/catalog.svg',
+                    },
+                    {
+                        content: 'Create',
+                        value: '/filter',
+                        icon: './img/catalog.svg',
+                        children: [
+                            {
+                                content: 'Library',
+                                value: '/filter/saved-filter',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'Build',
+                                value: '/filter/build-filter',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'Set Ops',
+                                value: '/filter/set-ops',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'Node Ops',
+                                value: '/filter/scope-ops',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'Duplicate',
+                                value: '/filter/duplicate-ops',
+                                icon: './img/catalog.svg',
+                            },
+                        ],
+                    },
+                    {
+                        content: 'Enhance',
+                        value: '/enhance',
+                        icon: './img/catalog.svg',
+                    },
+                    {
+                        content: 'Action',
+                        value: '/action',
+                        icon: './img/catalog.svg',
+                    },
+                    {
+                        content: 'Report',
+                        value: '/report',
+                        icon: './img/catalog.svg',
+                        children: [
+                            {
+                                content: 'System',
+                                value: '/report/system-reports',
+                                icon: './img/catalog.svg',
+                            },
+                            {
+                                content: 'User',
+                                value: '/report/user-reports',
+                                icon: './img/catalog.svg',
+                            },
+                        ],
+                    },
+                    {
+                        content: 'Exceptions',
+                        value: '/exceptions',
+                        icon: './img/catalog.svg',
+                    },
+                ],
+            })
+            // .appendTo('#qunit-fixture');
             .appendTo('body');
         ok(bc);
         $('[id^=qunit-]').hide();
         $('body').css('margin', 0);
     });
-    // test ('instantiation', function(){
-    //     var bc = VerticalNav()
-    //         .appendTo('body');
-    //     ok(bc);
-    //     $('[id^=qunit-]').hide();
-    //     $('body').css('margin', 0);
-    // });
 
-    // test ('one crumb ahaha', function(){
-    //     var bc = BreadCrumb({
-    //         crumbs: [
-    //             {value: 'foo', content: 'Foo'}
-    //         ]
-    //     }).appendTo('body');
-    //     equal(bc.get('crumbs').length, 1, 'one crumb');
-    // });
-
-    // test ('two crumbs ahaha', function(){
-    //     var bc = BreadCrumb({
-    //         crumbs: [
-    //             {value: 'foo', content: 'Foo'},
-    //             {value: 'bar', content: 'Bar'}
-    //         ]
-    //     });
-    //     equal(bc.get('crumbs').length, 2, 'two crumbs');
-    // });
-
-    // test ('three crumbs ahaha', function(){
-    //     var bc = BreadCrumb({
-    //         crumbs: [
-    //             {value: 'foo', content: 'Foo'},
-    //             {value: 'bar', content: 'Bar'},
-    //             {value: 'tao', content: 'Tao'}
-    //         ]
-    //     });
-    //     equal(bc.get('crumbs').length, 3, 'three crumbs');
-    // });
-
-    // asyncTest ('breadcrumb click handler override', function(){
-    //     var clicks = 0,
-    //         BC = BreadCrumb.extend({
-    //             defaults: {
-    //                 crumbs: [
-    //                     {value: 'foo', content: 'Foo'},
-    //                     {value: 'bar', content: 'Bar'},
-    //                     {value: 'tao', content: 'Tao'}
-    //                 ]
-    //             },
-    //             onBreadCrumbClick: function(evt) {
-    //                 clicks++;
-    //             }
-    //         });
-    //         bc = BC();//.appendTo('body');
-
-    //     equal(clicks, 0, 'no clicks');
-    //     bc.$el.find('[value=foo]').click();
-    //     setTimeout(function() {
-    //         equal(clicks, 1, 'bar clicked');
-    //         bc.$el.find('[value=bar]').click();
-    //         setTimeout(function() {
-    //             equal(clicks, 2, 'foo clicked');
-    //             bc.$el.find('[value=tao]').click();
-    //             setTimeout(function() {
-    //                 equal(clicks, 2, 'tao is the last breadcrumb and cannot be clicked');
-    //                 start();
-    //             }, 10);
-    //         }, 10);
-    //     }, 10);
-    // });
-
-    // asyncTest ('breadcrumb click handler override returns crumb object', function(){
-    //     var BC = BreadCrumb.extend({
-    //             defaults: {
-    //                 crumbs: [
-    //                     {value: 'foo', content: 'Foo'},
-    //                     {value: 'bar', content: 'Bar'},
-    //                     {value: 'tao', content: 'Tao'}
-    //                 ]
-    //             },
-    //             onBreadCrumbClick: function(evt, crumb) {
-    //                 var $target = $(evt.currentTarget),
-    //                     value = $target.attr('value');
-    //                 equal(value, crumb.value, 'target value and crumb value match');
-    //             }
-    //         });
-    //         bc = BC().appendTo('body');
-
-    //     bc.$el.find('[value=foo]').click();
-    //     setTimeout(function() {
-    //         bc.$el.find('[value=bar]').click();
-    //         setTimeout(function() {
-    //             start();
-    //         }, 10);
-    //     }, 10);
-    // });
     start();
 });
